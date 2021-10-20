09797919bb24c22a40c37c98935ce332f73687b9
SrtaJulie <90550949+SrtaJulie@users.noreply.github.com>
2021-10-20 10:33:59 +0200

    Update Exo3Sae.java

--- Exo3Sae.java
+++ Exo3Sae.java
@@ -21,32 +21,31 @@ public class StringRLettre {
     
     /**
      * Le programme demande à l’utilisateur de saisir un mot.
-	 * Si le mot n’est pas vide le programme affichera la 1er lettre 
-	 * (ou le 1er caractère, car on ne vérifiera pas qu’il s’agit d’un mot) 
-	 * du mot.
-	 *
+     * Si le mot n’est pas vide le programme affichera la 1er lettre 
+     * (ou le 1er caractère, car on ne vérifiera pas qu’il s’agit d’un mot) 
+     * du mot.
+     *
      * @param args  argument non utilisé
      */
-        
-        public static void main(String[] args) { 
+       
+	public static void main(String[] args) { 
 
-			String motEntre; 
-			String caractere1;
+		String motEntre; 
+		Char caractere1;
 
-            Scanner saisieClavier = new Scanner(System.in);
-
-			System.out.print("Entrez un mot :");
-		    motEntre = saisieClavier.nextLine();
+        	Scanner saisieClavier = new Scanner(System.in);
+		System.out.print("Entrez un mot :");
+		motEntre = saisieClavier.nextLine();
 			
-			if (motEntre.isEmpty()){
-				System.out.println("Vous avez entré une chaîne vide. Recommencez.");				
+		if (motEntre.isEmpty()){
+			System.out.println("Vous avez entré une chaîne vide. Recommencez.");				
 				
-			} else {
-				caractere1 = motEntre.nextLine().charAt(0);
-				System.out.println("Le mot " + motEntre + " commence par la lettre " + caractere1 + " . ");
+		} else {
+			caractere1 = motEntre.charAt(0);
+			System.out.println("Le mot " + motEntre + " commence par la lettre " + caractere1 + " . ");
 
-			}	 
-		}
+		}	
+	}
 		
 
 }
