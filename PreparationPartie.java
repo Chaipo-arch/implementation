dd25f0656c02e79599b0287442c508811ed638bf
SrtaJulie <90550949+SrtaJulie@users.noreply.github.com>
2021-10-22 11:34:44 +0200

    Update PreparationPartie.java

--- PreparationPartie.java
+++ PreparationPartie.java
@@ -1,4 +1,6 @@
 /*
- *
- *
+ *  Ecrire un programme qui effectue la préparation d’une partie entre 2 joueurs, en saisissant les 
+ * noms, le nombre de tours souhaités et le niveau de difficulté. Si l’une des informations entrées est 
+ * incorrecte, le programme en informera l’utilisateur. Si par contre, elles sont toutes correctes, un message 
+ * de confirmation sera affiché.
  */
