bd568db257fdd638b9e4feadb5e2a7b9524f88b3
SrtaJulie <90550949+SrtaJulie@users.noreply.github.com>
2021-10-29 09:23:50 +0200

    Update PreparationPartie.java

--- PreparationPartie.java
+++ PreparationPartie.java
@@ -37,32 +37,15 @@ public class PreparationPartie{
         Scanner entree = Scanner(System.in);
         
         System.out.print("veuillez entrez le pseudo du joueur 1 : ");
-		
-		if (entree.isBlank) {
-			pseudoUn = entree.next() ;
+	pseudoUn = entree.next() ;
+		if (pseudoUn.isBlank()) {
 			entree.nextLine();
 		
 			System.out.print("veuillez entrez le pseudo du joueur 2 : ");
-		
-			if (entree.isBlank) {
-				pseudoDeux = entree.next() ;
+			pseudoDeux = entree.next() ;
+			if (pseudoDeux.isBlank()) {
 				entree.nextLine();
 		
-				System.out.print("veuillez entrez le nombre de tour : ");
-
-				if (entree.hasNextInt() || entree.nextInt < 4) {
-					nbTour = entree.nextInt();
-		
-					System.out.print("veuillez entrer le niveau de difficulté "
-									 + "(rappel : entrez 1 pour le niveau facile et 2 pour le niveau difficile)");
-					if (entree.hasNextInt() || entree.nextInt == 1 || entree.nextInt == 2) {
-						niveauDifficulte = entree.nextInt();
-					} else {
-						System.out.println("Erreur, entrez 1 ou 2 pour choisir le niveau de difficulté.");
-					}
-				} else {
-					System.out.println("Erreur, entrez un entier supérieur ou égal à 4.");				
-				}
 			} else {
 				System.out.println("Erreur, entrez le pseudo du joueur 2");
 			}
