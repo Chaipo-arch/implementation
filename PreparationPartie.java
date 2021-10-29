3880406ed3e93cff6f1ab7b2e885fa2702fa97e9
SrtaJulie <90550949+SrtaJulie@users.noreply.github.com>
2021-10-29 09:37:35 +0200

    Update PreparationPartie.java

--- PreparationPartie.java
+++ PreparationPartie.java
@@ -51,6 +51,27 @@ public class PreparationPartie{
 		System.out.println("Erreur, entrez le pseudo du joueur 1 : ");
 		pseudoDeux = entree.next();
 		entree.nextLine();
+	}
+	    
+	System.out.print("veuillez entrez le nombre de tour : ");
+	nbTour = entree.nextInt();
+	entree.nextInt();
+	    
+	while (entree.hasNextInt() || entree.nextInt < 4) {
+		System.out.println("Erreur, entrez un entier supérieur ou égal à 4. Recommencez :");
+		nbTour = entree.nextInt();
+		entree.nextInt();
+					
+	}
+	System.out.print("veuillez entrer le niveau de difficulté "
+			 + "(rappel : entrez 1 pour le niveau facile et 2 pour le niveau difficile)");
+	niveauDifficulte = entree.nextInt();
+	entree.nextInt();
+	
+	while (entree.hasNextInt() || entree.nextInt == 1 || entree.nextInt == 2) {
+		System.out.println("Erreur, entrez 1 ou 2 pour choisir le niveau de difficulté, recommencez : ");
+					
 		
 	}
+    }
 }
